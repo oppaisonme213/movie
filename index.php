5666fdc519e4f3f4a30286fdadf3c297a416c1d1
James Allen Maye III (Akarian) <jamesmaye6094@gmail.com>
2021-04-23 13:27:34 +0800

    add header images

--- index.php
+++ index.php
@@ -18,8 +18,36 @@
     <?php
       require 'templates/navigation_bar.php';
     ?>
+    <div>
+      <div id="slider1" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="d-block w-100" alt="First slide"></div>
+          </div>
 
-    
+          <div class="carousel-item">
+            <div class="d-block w-100" alt="Second slide"></div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="d-block w-100" alt="Third slide"></div>
+          </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#slider1" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#slider1" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
   </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- templates/navigation_bar.php
+++ templates/navigation_bar.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand ml-5" href="#"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-photo d-block text-center fa-lg"></i>Movies</a>
+        <a class="nav-link" href="#"><i class="fa fa-film d-block text-center fa-lg"></i>Movies</a>
       </li>
 
       <li class="nav-item">
