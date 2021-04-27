23f2ebc04c6856db974bb01d60eb31b789b814b9
joshua fernando <jay.fernando.m@gmail.com>
2021-04-27 11:48:48 +0800

    linux update configuration

--- class/config.php
+++ class/config.php
@@ -2,12 +2,12 @@
 
   class config {
     private $username = 'root';
-    private $password = '';
+    private $password = 'admin123';
     public $pdo = null;
 
     public function con(){
       try{
-        $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=seat_registration_system;port=3306', $this->username, $this->password);
+        $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=seat_registration_system', $this->username, $this->password);
       } catch (PDOException $e) {
         die($e);
       }
--- php/init.php
+++ php/init.php
@@ -3,7 +3,7 @@
 require_once 'function.php';
 
 spl_autoload_register(function($class) {
-  require_once $_SERVER['DOCUMENT_ROOT']."/final/class/".$class.'.php';
+  require_once $_SERVER['DOCUMENT_ROOT']."/class/".$class.'.php';
 });
 
 ?>
